c36c5cd4d34f87cc10a05f8d69f49bb5b860ae7e
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2015-02-23 10:00:41 +1300

    Works for Single Order Print

---
+++ app/code/PrintOrderPdf/Block/Adminhtml/PrintOrderButton.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @author     Kristof Ringleff
+ * @package    Fooman_PrintOrderPdf
+ * @copyright  Copyright (c) 2015 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Fooman\PrintOrderPdf\Block\Adminhtml;
+
+class PrintOrderButton extends \Magento\Backend\Block\Widget\Container
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    public function __construct(
+        \Magento\Backend\Block\Widget\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    protected function _construct()
+    {
+        $this->addButton(
+            'fooman_print',
+            [
+                'label'   => 'Print',
+                'class'   => 'print',
+                'onclick' => 'setLocation(\'' . $this->getPdfPrintUrl() . '\')'
+            ]
+        );
+
+        parent::_construct();
+    }
+
+    /**
+     * @return string
+     */
+    public function getPdfPrintUrl()
+    {
+        return $this->getUrl('fooman_printorderpdf/order/print/order_id/' . $this->getOrderId());
+    }
+
+    /**
+     * @return integer
+     */
+    public function getOrderId()
+    {
+        return $this->_coreRegistry->registry('sales_order')->getId();
+    }
+}
