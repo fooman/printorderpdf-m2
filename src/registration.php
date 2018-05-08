97b76e9ca80b4e74610e1d7be240de8f5342f3a7
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2018-05-08 14:01:18 +1200

    Code moved into implementation package

--- src/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Fooman_PrintOrderPdf',
-    __DIR__
-);
--- tests/unit/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require (__DIR__.'/../../vendor/autoload.php');
-
-putenv("PRE_BASE_DIR=" . __DIR__ . "/Test");
-$runner = new \Fooman\PhpunitPolyfill\Runner();
-$runner->execute();
